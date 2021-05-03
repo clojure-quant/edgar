45bb408b7dad2831e87c29a3dd02de589475b11d
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-05-02 19:53:38 -0500

    analysis

--- src/edgar/analysis.clj
+++ src/edgar/analysis.clj
@@ -1,3 +1,45 @@
+(ns edgar.analysis
+  (:require
+   [edgar.edn :refer [edn-read]]
+   ))
+
+ (defn nport-file [{:keys [cik no]} {:keys [fid]}]
+   (str "data/nport/" cik "_" no "_" fid ".edn"))
+
+ (defn load-one [f]
+   (let [f (nport-file f f)]
+     (edn-read f)))
+
+ (defn load-x []
+   (let [a (load-one {:cik "100334"
+                      :no "0001145549-20-036689"
+                      :fid "S000006192"})
+         b (load-one {:cik "100334"
+                      :no "0001145549-20-076715"
+                      :fid "S000006192"})
+         c (load-one {:cik "100334"
+                      :no "0001145549-20-016610"
+                      :fid "S000006192"})
+         ]
+     {:a a
+      :b b
+      :c c}
+     )  
+   )
+
+; :cusip "N/A"
+
+ (defn info [pf]
+   {:nav (:nav pf)
+    :ct (count (:holdings pf))
+    }
+   )
+
+ (->> (load-x)
+      vals
+     (map info  )
+  
+  )
 
 
- :cusip "N/A"
+
--- src/edgar/demo.clj
+++ src/edgar/demo.clj
@@ -1,13 +1,15 @@
 (ns edgar.demo
   (:require
    [clojure.core.async :refer [thread go <!]]
+   [clojure.java.io]
+
    [edgar.edn :refer [edn-read edn-save]]
    [edgar.download :refer [dl-filing dl-primary]]
    [edgar.index :refer [filings-index]]
    [edgar.filing :refer [parse-filing]]
    [edgar.portfolio :refer [extract-pf-str]])
   ;(:gen-class)
-  )
+  (:import java.io.File))
 
 (defn filing-file [{:keys [cik no]}]
   (str "data/filings/" cik "-" no "-index.html"))
@@ -34,39 +36,67 @@
 
 (defn get-nport-index [filename]
   (println "getting index.." filename)
-  (let [filings (filings-index filename  "NPORT-P"  1022804 )]; 100334
+  (let [filings (filings-index filename  "NPORT-P"  1022804)]; 100334
     (edn-save "data/index.edn" filings)
     (println "downloading data.. #:" (count filings))
     (doall (map get-nport filings))))
 
 
 (defn get-nport-all []
-   (doall (for [year [2018 2019 2020]
-         q [1 2 3 4]]
+  (doall (for [year [2018 2019 2020]
+               q [1 2 3 4]]
      ; "data/index/2020-QTR4.tsv"
      ; "data/index/2021-QTR1.tsv"
-     (let [filename (str "data/index/" year "-QTR" q ".tsv")]
-     (get-nport-index filename)
-     ))))
+           (let [filename (str "data/index/" year "-QTR" q ".tsv")]
+             (get-nport-index filename)))))
 
 
 
-(defn -main
-  []
-
+(defn job1 []
   (thread
     (get-nport {:cik 1004655
-                :no "0001752724-21-069935"}))
+                :no "0001752724-21-069935"})))
 
+(defn job2 []
   (go  (<! (thread
              (println "start..")
              ;(get-nport-index "data/index/2020-QTR4.tsv")
              (get-nport-all)
              ;
              ))
-       (println "done."))
+       (println "done.")))
 
+(def re-nport #"(.*)_(.*)_(.*)\.edn")
+(defn nport-info [f]
+  (let [s (.getName f)
+        m (re-find re-nport s)
+        [_ cik no fid] m]
+    {:cik cik
+     :no no
+     :fid fid}))
 
+(defn f []
+  (let [dir (clojure.java.io/file "data/nport")
+        files (.listFiles dir)
+        f (first files)
+        
+        ]
+    (->>
+    (map nport-info files)
+    (sort-by :fid)
+     (filter #(= "S000006192" (:fid %))) 
+    ;(.getName f)
+    )))
+
+(defn -main []
+
+  ;
+  )
+
+(comment
+  (job1)
+  (job2)
+  (f)
   ;
   )
 
